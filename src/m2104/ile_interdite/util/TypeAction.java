f44784eb4334caab4f7c24ce8a12e2d94dc53279
capelth <capelth@pc-dg-033-06.iut2.upmf-grenoble.fr>
2020-01-10 11:24:20 +0100

    class enum

---
+++ src/m2104/ile_interdite/util/TypeAction.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m2104.ile_interdite.util;
+
+/**
+ *
+ * @author capelth
+ */
+public enum TypeAction {
+    
+    BOUGER,
+    ASSECHER,
+    DONNER,
+    RECUPERER_TRESOR,
+    TERMINER,
+    RECEVOIR,
+    CHOISIR_CARTE,
+    CHOISIR_TUILE,
+    INITIALISER;
+    
+}
